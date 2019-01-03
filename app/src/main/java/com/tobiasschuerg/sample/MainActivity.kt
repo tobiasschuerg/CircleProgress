b26a2cfece0c372c9f649293c1f1ff102058de69
Tobias Schürg <tobiasschuerg@gmail.com>
2019-01-03 17:05:29 +0100

    Migration to AndroidX.

--- app/src/main/java/com/tobiasschuerg/sample/MainActivity.kt
+++ app/src/main/java/com/tobiasschuerg/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tobiasschuerg.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.progress
 import kotlinx.android.synthetic.main.activity_main.reset_button
--- progresscircle/src/main/java/com/tobiasschuerg/progresscircle/ProgressCircleView.kt
+++ progresscircle/src/main/java/com/tobiasschuerg/progresscircle/ProgressCircleView.kt
@@ -13,8 +13,8 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.RectF
 import android.os.Build
-import android.support.annotation.RequiresApi
-import android.support.v4.content.ContextCompat
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
